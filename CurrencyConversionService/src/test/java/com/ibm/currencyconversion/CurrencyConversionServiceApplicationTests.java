2f973cb1b5b871bc813ce10bfd557f1637849674
bsharm29 <65173768+bsharm29@users.noreply.github.com>
2020-11-05 23:07:19 +0530

    Add files via upload

---
+++ CurrencyConversionService/src/test/java/com/ibm/currencyconversion/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ibm.currencyconversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+	/*
+	 * @Test void contextLoads() { }
+	 */
+
+}
